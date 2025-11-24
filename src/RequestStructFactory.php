22f1e85d0983a7f5fd261ad09512adeb78a8978a
Paul M. Jones <pmjones@pmjones.io>
2025-11-24 12:58:43 -0600

    per research, RequestStructFactory() should return an instance based on the current request

--- src/RequestStructFactory.php
+++ src/RequestStructFactory.php
@@ -3,45 +3,7 @@ declare(strict_types=1);
 
 namespace RequestInterop\Interface;
 
-use StreamInterop\Interface\StringableStream;
-use UploadInterop\Interface\UploadTypeAliases;
-use UriInterop\Interface\UriStruct;
-
-/**
- * @phpstan-import-type request_body_array from RequestTypeAliases
- *
- * @phpstan-import-type request_cookies_array from RequestTypeAliases
- *
- * @phpstan-import-type request_headers_array from RequestTypeAliases
- *
- * @phpstan-import-type request_method_string from RequestTypeAliases
- *
- * @phpstan-import-type request_query_array from RequestTypeAliases
- *
- * @phpstan-import-type request_server_array from RequestTypeAliases
- *
- * @phpstan-import-type uploads_array from UploadTypeAliases
- */
 interface RequestStructFactory
 {
-    /**
-     * @param ?request_cookies_array $cookies
-     * @param ?request_headers_array $headers
-     * @param ?request_body_array $body
-     * @param ?request_method_string $method
-     * @param ?request_query_array $query
-     * @param ?request_server_array $server
-     * @param ?uploads_array $uploads
-     */
-    public function newRequest(
-        ?array $body = null,
-        ?array $cookies = null,
-        ?array $headers = null,
-        ?StringableStream $input = null,
-        ?string $method = null,
-        ?array $query = null,
-        ?array $server = null,
-        ?array $uploads = null,
-        ?UriStruct $uri = null,
-    ) : RequestStruct;
+    public function newRequest() : RequestStruct;
 }
